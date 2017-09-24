c37062e38fdc4d82c5c5e651ee70029a9a1ae997
郑一星 <zhengyixing@dyn-160-39-9-5.dyn.columbia.edu>
2017-09-23 20:53:02 -0400

    combine all the resources.

--- NmatHeat2D.m
+++
@@ -1,6 +0,0 @@
-% Shape function 
- 
-function N = NmatHeat2D(eta,psi)
-       
-        N  = 0.25 * [(1-psi)*(1-eta)  (1+psi)*(1-eta)  (1+psi)*(1+eta)  (1-psi)*(1+eta)]; % shape functions
-
--- VP_line.m
+++
@@ -1,8 +0,0 @@
-clear all;
-close all;
-%draw the four figures in question 8
-VP_lineFGH;
-VP_line_EE;
-VP_line_FF;
-VP_line_GG;
-
